99d0eb512501e2a4de888a8289650a832e067337
rapha1005 <106275964+rapha1004@users.noreply.github.com>
2025-06-24 16:49:51 +0200

    fix

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,25 @@
 import { useEffect, useState } from "react";
 import Project from "./Project";
+import work from "../assets/work.json"
+
 
 export default function component() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    async function getData() {
-      const res = await fetch("/data/work.json");
-      const data = await res.json();
+  // useEffect(() => {
+  //   async function getData() {
+  //     const res = await fetch("/data/work.json");
+  //     const data = await res.json();
 
-      if (data.length) {
-        setData(data);
-      }  
-    }
+  //     if (data.length) {
+  //       setData(data);
+  //     }  
+  //   }
 
-    getData();
-  }, []);
+  //   getData();
+  // }, []);
 
-  console.log(data);
+  // console.log(data);
 
   return (
     <section className="pt-20 w-[90%] mx-auto mb-20">
@@ -25,9 +27,9 @@ export default function component() {
         <h2 className="text-white font-bold text-5xl lg:text-[88px]">Projects</h2>
         <a href="#" className="underline tracking-widest text-white decoration-[#4EE1A0] underline-offset-8 decoration-[2px] font-bold hover:text-[#4EE1A0] duration-150">CONTACT ME</a>
       </div>
-      {data && data.length ? (
+      {work && work.length ? (
         <ul className="grid gap-8 grid-cols-1 sm:grid-cols-2">
-          {data.map((project) => (
+          {work.map((project) => (
             <Project key={project.titregrid}
               link={project.link}
               description={project.descriptiontitre}
